be7ca1a040ebd01e8f00743a6a6cfb615480fc7f
Max <maxrodaspc@gmail.com>
2025-05-24 23:01:47 -0400

    modificado

--- php/autenticar.php
+++ php/autenticar.php
@@ -5,20 +5,25 @@ include("conexion.php");
 $correo = $_POST['correo'];
 $password = sha1($_POST['password']);
 
-$stmt = $conexion->prepare('SELECT correo, nombre, rol, estado FROM usuarios WHERE correo=? AND password=?'); // <-- AÑADIDO estado
+$stmt = $conexion->prepare('SELECT correo, nombre, rol, estado FROM usuarios WHERE correo=? AND password=?');
 $stmt->bind_param("ss", $correo, $password);
 $stmt->execute();
 
 $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc(); 
-    echo "Usuario encontrado";
+    $row = $result->fetch_assoc();
     $_SESSION['correo'] = $correo;
     $_SESSION['rol'] = $row['rol'];
-    $_SESSION['estado'] = $row['estado']; 
-    header("Location:../admin/Example2/example2.html");
+    $_SESSION['estado'] = $row['estado'];
+    $_SESSION['id'] = $row['id'];
+
+    if ($row['rol'] === 'admin') {
+        header("Location:../admin/Example/example2.html");
+    } else {
+        header("Location:../user/userPage.html");
+    }
+    exit();
 } else {
     echo "Error datos de autenticación incorrectos";
-    
 }
